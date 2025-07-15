907192457553dd1cde65d7b303f53572ae818fb5
Muhammad Awais <ows4444@gmail.com>
2025-07-15 23:37:29 +0500

    refactor(validation): streamline validation process by merging results and enhancing business rule validation

--- src/modules/dynamic-dto/application/pipelines/validation.pipeline.ts
+++ src/modules/dynamic-dto/application/pipelines/validation.pipeline.ts
@@ -5,6 +5,7 @@ import { DynamicSchemaEntity } from '../../domain/entities/dynamic-schema.entity
 import { SchemaValidationPipeline } from './schema-validation.pipeline';
 import { ValidationResult } from '../../core/interfaces/validation';
 import { ValidationIssue } from '../../core/interfaces/validation/validation-issue.interface';
+import { ValidationResultMerger } from '../../core/utils/validation-result-merger';
 
 @Injectable()
 export class ValidationPipeline {
@@ -14,24 +15,11 @@ export class ValidationPipeline {
   ) {}
 
   validate(schema: DynamicSchemaEntity): ValidationResult {
-    // Use the new enhanced validation pipeline
     const enhancedResult = this.schemaValidationPipeline.execute(schema);
+    const originalResult = this.schemaValidator.validate(schema.properties);
+    const businessRulesResult = this.validateBusinessRules(schema);
 
-    // Fallback to original validation if enhanced fails
-    if (!enhancedResult.isValid) {
-      const originalResult = this.schemaValidator.validate(schema.properties);
-
-      // Merge results
-      return {
-        isValid: enhancedResult.isValid && originalResult.isValid,
-        infos: [...(enhancedResult.infos || []), ...(originalResult.infos || [])],
-        issues: [...(enhancedResult.issues || []), ...(originalResult.issues || [])],
-        errors: [...(enhancedResult.errors || []), ...(originalResult.errors || [])],
-        warnings: [...(enhancedResult.warnings || []), ...(originalResult.warnings || [])],
-      };
-    }
-
-    return enhancedResult;
+    return ValidationResultMerger.mergeResults([enhancedResult, originalResult, businessRulesResult]);
   }
 
   private validateBusinessRules(schema: DynamicSchemaEntity): ValidationResult {
--- src/modules/dynamic-dto/dynamic-dto.module-definition.ts
+++ src/modules/dynamic-dto/dynamic-dto.module-definition.ts
@@ -4,13 +4,5 @@ import { DynamicDtoModuleOptions } from './interfaces/module-options.interface';
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } = new ConfigurableModuleBuilder<DynamicDtoModuleOptions>({
   moduleName: 'DynamicDto',
 })
-  .setExtras(
-    {
-      isGlobal: false,
-    },
-    (definition, extras) => ({
-      ...definition,
-      global: extras.isGlobal,
-    }),
-  )
+  .setExtras({ isGlobal: false }, (definition, extras) => ({ ...definition, global: extras.isGlobal }))
   .build();
